321b5c838f21f192f3d0734a96bdb4bcef5b6e37
Christiana Vandermale <christianavdm@gmail.com>
2018-04-24 00:51:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,6 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  position = katz_deli
+  position = katz_deli.index(name)
   puts "Welcome, #{name}. You are number __ in line."
 end

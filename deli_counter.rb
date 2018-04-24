eb93b7106de45037bf617a5354f3302e793358e7
Christiana Vandermale <christianavdm@gmail.com>
2018-04-24 00:40:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,27 @@
 katz_deli = []
 
-
+def assign_rooms(list)
+  room = 1 
+  room_assignments = []
+  list.each do |name|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  return room_assignments
+end
 
 
 def line(katz_deli) 
   if katz_deli.count == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently: "
     counter = 1
+    line_positions = []
     katz_deli.each do |name|
-      puts "#{counter}. #{name} "
+      line_positions << "#{counter}. #{name} "
       counter += 1
     end
+    puts "The line is currently: "
   end
 end
 

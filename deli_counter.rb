8dea371f10882bae3801d7ca6015b1e78381e09f
Christiana Vandermale <christianavdm@gmail.com>
2018-04-24 01:00:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,11 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.count == 0 
+    puts "The line is currently empty."
+  else 
   puts "Currently serving #{katz_deli[0]}."
   katz_deli.shift
+end
 end
   

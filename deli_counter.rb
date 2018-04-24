ab79da4a510f629df55b5663660ab2c797ab6897
Christiana Vandermale <christianavdm@gmail.com>
2018-04-24 00:45:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     counter = 1
-    line_positions = ["The line is currently: "]
+    line_positions = ["The line is currently:"]
     katz_deli.each do |name|
-      line_positions << "#{counter}. #{name} "
+      line_positions << "#{counter}. #{name}"
       counter += 1
     end
     puts line_positions.join
